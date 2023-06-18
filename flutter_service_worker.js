d1fd386277a722dfe1bc0c1baf442e9937e826e5
waleed hassanin <snowflakecodev12022@gmail.com>
2023-06-18 20:59:58 +0300

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "7bd46033a147c8d33a1d572af3ad7af4",
-"index.html": "c16d5c38b03b13c1a20a2376bb653e10",
-"/": "c16d5c38b03b13c1a20a2376bb653e10",
-"main.dart.js": "a0d444777da061374ff42740ba023e5e",
+"index.html": "28b69821443f4d31d9c785a73dd72840",
+"/": "28b69821443f4d31d9c785a73dd72840",
+"main.dart.js": "ef3b1201a8452f77c0e40af46bc57872",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
